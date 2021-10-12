96b20a4e399cc43c5f2f5bf6c7e663d7b0e6df2b
kanocence <1051641454@qq.com>
2021-10-12 15:24:51 +0800

    :sparkles:测试模块

---
+++ src/modules/message/hello.ts
@@ -0,0 +1,25 @@
+import { Injectable } from "@nestjs/common"
+import { GroupMessageEventData, MessageModule } from "src"
+import { messageOp } from "src/utils/event"
+import { BotService } from "src/service/bot.service"
+
+@Injectable()
+export class HelloModule implements MessageModule {
+
+  type: 'group'
+
+  constructor(private readonly botService: BotService) { }
+
+  validator(msg: GroupMessageEventData): boolean {
+    return messageOp(msg.message, '在吗')
+  }
+
+  processor(msg: GroupMessageEventData): void {
+    this.botService.send('send_group_msg',
+      {
+        group_id: msg.group_id,
+        message: 'w'
+      },
+      msg)
+  }
+}
