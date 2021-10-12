e237a3e0892d35f869651f52fde54894914784bd
kanocence <1051641454@qq.com>
2021-10-12 15:26:43 +0800

    :sparkles:测试模块

--- src/modules/message/index.ts
+++ src/modules/message/index.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@nestjs/common";
+import { HelloModule } from "./hello";
 import { BiliLiveTaskModule } from "./live.task";
 import { RepeatModule } from "./repeat";
 import { RestartModule } from "./restart";
@@ -12,15 +13,15 @@ export class RegisterMessageModules {
   constructor(
     private biliTaskModule: BiliLiveTaskModule, private setuModule: SetuModule,
     private restartModule: RestartModule, private repeatModule: RepeatModule,
-    private biliSpaceTaskModule: BiliSpaceTaskModule
+    private biliSpaceTaskModule: BiliSpaceTaskModule, private helloModule: HelloModule
   ) { }
 
   register() {
     return [this.biliTaskModule, this.setuModule, this.restartModule, this.repeatModule,
-    this.biliSpaceTaskModule]
+    this.biliSpaceTaskModule, this.helloModule]
   }
 }
 
 /** 导出message模块 */
 export const MessageModules = [RegisterMessageModules, BiliLiveTaskModule, SetuModule, RestartModule,
-  RepeatModule, BiliSpaceTaskModule]
+  RepeatModule, BiliSpaceTaskModule, HelloModule]
