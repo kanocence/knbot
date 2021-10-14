dc1829febd47e5846b69e2d4e53e360de9b39ca5
kanocence <1051641454@qq.com>
2021-10-14 15:18:19 +0800

    :sparkles:增加配置日志等级

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,9 @@ import { AppModule } from './app.module'
 import { WsAdapter } from './websocket/ws.adapter'
 
 async function bootstrap() {
-  const { server } = global.config
+  const { server, log } = global.config
   new Logger('bootstrap').log('The api runs on port :>> ' + server['api-port'])
-
-  const app = await NestFactory.create(AppModule, { logger: ['debug', 'error', 'warn', 'log', 'verbose'] })
+  const app = await NestFactory.create(AppModule, { logger: log })
   app.useWebSocketAdapter(new WsAdapter(app))
   await app.listen(server['api-port'])
 }
