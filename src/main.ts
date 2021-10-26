32ad8b7ee417efb10b408ebbe8b84cac565017d0
kanocence <1051641454@qq.com>
2021-10-26 10:00:09 +0800

    :sparkles:添加请求拦截器

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { HttpExceptionFilter } from './filters/http-exception.filter'
+import { TransformInterceptor } from './interceptor/transform.interceptor'
 import { WsAdapter } from './websocket/ws.adapter'
 
 async function bootstrap() {
@@ -8,6 +10,10 @@ async function bootstrap() {
   new Logger('bootstrap').log('The api runs on port :>> ' + server['api-port'])
   const app = await NestFactory.create(AppModule, { logger: log })
   app.useWebSocketAdapter(new WsAdapter(app))
+  // 注册错误的过滤器
+  app.useGlobalFilters(new HttpExceptionFilter())
+  // 注册请求拦截器
+  app.useGlobalInterceptors(new TransformInterceptor())
   await app.listen(server['api-port'])
 }
 bootstrap()
