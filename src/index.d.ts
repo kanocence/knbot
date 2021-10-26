b03d53f5df7d57bb27d0f71c96b7ce0ccb18f916
kanocence <1051641454@qq.com>
2021-10-26 14:45:10 +0800

    :arrow_up:定义接口返回类型

--- src/index.d.ts
+++ src/index.d.ts
@@ -555,3 +555,25 @@ export interface ArrayMessageJson extends Message {
   type: 'json'
   data: { 'data': string }
 }
+
+export type OpenApiRes<T> = {
+  data: T[],
+  retcode: number,
+  status: string
+}
+
+export type GroupListRes = {
+  group_create_time: number,
+  group_id: number,
+  group_level: number,
+  group_memo: string,
+  group_name: string,
+  max_member_count: number,
+  member_count: number
+}
+
+export type FriendListRes = {
+  nickname: string,
+  remark: string,
+  user_id: number
+}
